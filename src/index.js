3e88ff9234bf225f18a72913cbfc175cd2f5ec16
zoeferencova <zoeferencova@gmail.com>
2020-05-01 10:59:07 -0400

    added font

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
+import "./fonts/Apercu-Font/ApercuLight.otf";
 import "./index.css";
+
 
 ReactDOM.render(
     <BrowserRouter>
