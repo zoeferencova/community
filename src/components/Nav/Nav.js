f36718a8d02456f9f11b807140f5798aa591d4c3
zoeferencova <zoeferencova@gmail.com>
2020-05-01 11:17:27 -0400

    added basic nav bar styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,16 +7,16 @@ export default class Nav extends Component {
     render() {
         return (   
             <nav className={styles.nav}>
-                <NavLink to={this.props.isLoggedIn ? "/home" : "/"}><img src={require("../../images/placeholder-logo.png")} alt="logo" className={styles.logo} /></NavLink>
+                <NavLink to={this.props.isLoggedIn ? "/home" : "/"}><img src={require("../../images/logo-placeholder.png")} alt="logo" className={styles.logo} /></NavLink>
                 {this.props.isLoggedIn ? 
                     <ul className={styles.navLinks}>
-                        <li><NavLink to="/messages"><i className="far fa-comment"></i> Messages</NavLink></li>
-                        <li><NavLink to="/account"><i className="far fa-user"></i> Account</NavLink></li>
+                        <NavLink to="/messages"><li><i className="far fa-comment"></i> Messages</li></NavLink>
+                        <NavLink to="/account"><li><i className="far fa-user"></i> Account</li></NavLink>
                     </ul>
                 :
                     <ul className={styles.navLinks}>
-                        <li><NavLink to="/login">Log In</NavLink></li>
-                        <li><NavLink to="/register">Sign Up</NavLink></li>
+                        <NavLink to="/login"><li className={styles.logIn}>Log In</li></NavLink>
+                        <NavLink to="/register"><li className={styles.signUp}>Sign Up</li></NavLink>
                     </ul>
                 } 
             </nav>  
