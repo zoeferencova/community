16732c0042bffc216ed82f38be165fe0a077d4e4
zoeferencova <zoeferencova@gmail.com>
2020-05-19 13:49:28 -0400

    added registration functionality

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 
+import AuthApiService from "../../services/auth-api-service";
+
 class RegistrationForm extends Component {    
-    handleSubmit(e) {
+    state = { error: null };
+    
+    handleSubmit = e => {
         e.preventDefault();
-        this.props.history.push("/login")
+        const { first_name, email, password, confirm_password } = e.target;
+        console.log(first_name.value)
+        password.value !== confirm_password.value
+            ? this.setState({ error: "Passwords do not match" })
+            : AuthApiService.postUser({
+                first_name: first_name.value,
+                email: email.value,
+                password: password.value
+            })
+                .then(user => {
+                    first_name.value = "";
+                    email.value = "";
+                    password.value = "";
+                    confirm_password.value = "";
+                    this.props.history.push("/login");
+                })
+                .catch(res => 
+                    this.setState({ error: res.error })
+                )
     }
     
     render() {
         return (
             <form onSubmit={e => this.handleSubmit(e)}>
+                <div role="alert">
+                    {this.state.error && <p>{this.state.error}</p>}
+                </div>
                 <div>
-                    <label htmlFor="first-name">First name</label>
-                    <input required type="text" name="first-name" id="first-name" />
+                    <label htmlFor="first_name">First name</label>
+                    <input required type="text" name="first_name" id="first_name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
@@ -23,8 +48,8 @@ class RegistrationForm extends Component {
                     <input required type="password" name="password" id="password" />
                 </div>
                 <div>
-                    <label htmlFor="confirm-password">Confirm Password</label>
-                    <input required type="password" name="confirm-password" id="confirm-password" />
+                    <label htmlFor="confirm_password">Confirm Password</label>
+                    <input required type="password" name="confirm_password" id="confirm_password" />
                 </div>
                 <Link to="/"><button>Cancel</button></Link>
                 <button type="submit">Create Account</button>
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_BASE_URL,
+    // API_ENDPOINT: process.env.REACT_APP_BASE_URL,
+    API_ENDPOINT: "http://localhost:8000/api",
     TOKEN_KEY: process.env.TOKEN_KEY,
     GMAPS_API_KEY: process.env.REACT_APP_GMAP_API_KEY
 }
