e2dbaf5e2ccf29d44a4d40a2336f370bf1a7313f
zoeferencova <zoeferencova@gmail.com>
2020-05-20 17:31:35 -0400

    added edit user information functionality

--- src/routes/AccountPage/AccountPage.js
+++ src/routes/AccountPage/AccountPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 
 import TokenService from "../../services/token-service";
+import UserDataService from "../../services/user-data-service";
 import CommUnityContext from "../../contexts/context";
 import styles from "./AccountPage.module.css"
 
@@ -28,18 +29,31 @@ class AccountPage extends Component {
         this.props.history.push("/");
     }
 
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const { first_name, email } = this.state;
+        const userInfo = { first_name, email };
+        const userId = this.context.user.id;
+
+        UserDataService.patchUser(userInfo, userId)
+            .then(res => {
+                this.context.updateUser(userInfo)
+            })
+    }
+
     render() {
         return (   
             <main>
                 <h3>Account</h3>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={e => this.handleSubmit(e)}>
                     <div>
                         <label htmlFor="first_name">First Name</label>
-                        <input type="text" name="first_name" id="first_name" value={this.state.first_name} onChange={this.handleChangeName} />
+                        <input required type="text" name="first_name" id="first_name" value={this.state.first_name} onChange={this.handleChangeName} />
                     </div>
                     <div>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="email" value={this.state.email} onChange={this.handleChangeEmail} />
+                        <input required type="email" name="email" id="email" value={this.state.email} onChange={this.handleChangeEmail} />
                     </div>
                     <Link to="/home"><button>Cancel</button></Link>
                     <button type="submit">Save</button>
