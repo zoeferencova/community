453ad769f692d420c94b66098b4bbe896c2d9176
zoeferencova <zoeferencova@gmail.com>
2020-06-10 19:13:32 -0400

    fixed arrow issue

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     API_ENDPOINT: process.env.REACT_APP_BASE_URL,
-    SOCKET_URL: process.env. REACT_APP_SOCKET_URL,
+    SOCKET_URL: process.env.REACT_APP_SOCKET_URL,
     // API_ENDPOINT: "http://localhost:8000/api",
     TOKEN_KEY: process.env.TOKEN_KEY,
     GMAPS_API_KEY: process.env.REACT_APP_GMAP_API_KEY
