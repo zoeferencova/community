cb89b990ce53e1015c6ee39db851125d7c2f52e2
zoeferencova <zoeferencova@gmail.com>
2020-06-12 16:35:52 -0400

    added message info

--- src/components/MessageHeading/MessageHeading.js
+++ src/components/MessageHeading/MessageHeading.js
@@ -2,20 +2,28 @@ import React, { Component } from "react";
 import { CHAT_DELETED } from "../../message-utils/events";
 import ChatService from "../../services/chat-service";
 import CommUnityContext from "../../contexts/context";
+import MessageInfo from "../MessageInfo/MessageInfo";
 import styles from "../MessageLayout/MessageLayout.module.css";
 
 export default class MessageHeading extends Component {
     static contextType = CommUnityContext;
+
+    state = {
+        infoOpen: false
+    }
     
-    handleDelete(e) {
+    handleDelete = e => {
         e.preventDefault()
+        const chatId = this.context.activeChat.id;
         if (window.confirm('Are you sure you want to delete this chat? Your message history will be lost.')) {
-            ChatService.deleteChat(this.props.chatId)
-            .then(res => {
-                this.context.updateActiveChat(null)
-                this.context.removeChat(this.props.chatId)
-                this.context.socket.emit(CHAT_DELETED, { chatId: this.props.chatId, receiverId: this.props.receiver.id })
-            })
+            ChatService.deleteChat(chatId)
+                .then(res => {
+                    this.context.removeChat(chatId)
+                    this.context.socket.emit(CHAT_DELETED, { chatId: chatId, receiverId: this.props.receiver.id })
+                })
+                .then(res => {
+                    this.context.updateActiveChat(null)
+                })
         }
     }
 
@@ -24,20 +32,27 @@ export default class MessageHeading extends Component {
         this.context.updateActiveChat(null)
     }
 
+    toggleInfo = () => {
+        this.state.infoOpen ? this.setState({ infoOpen: false }) : this.setState({ infoOpen: true })
+    }
+
     render() {
         return (
-            <div className={styles.chatHeader}>
-                <div className={styles.userInfo}>
-                    <i className={`fas fa-chevron-left ${styles.chatBackArrow}`} onClick={this.props.mobileDisplayContacts}></i>
-                    <div className={styles.userName}>{this.props.receiver.first_name}</div>
-                    <div className={styles.status}>
-                        <div className={styles.indicator}></div>
+            <div className={styles.headerContainer}>
+                <div className={styles.chatHeader}>
+                    <div className={styles.userInfo}>
+                        <i className={`fas fa-chevron-left ${styles.chatBackArrow}`} onClick={this.props.mobileDisplayContacts}></i>
+                        <div className={styles.userName}>{this.props.receiver.first_name}</div>
+                        <div className={styles.status}>
+                            <div className={styles.indicator}></div>
+                        </div>
+                    </div>
+                    <div className={styles.options}>
+                        <i className="fas fa-info-circle" onClick={this.toggleInfo}></i>
+                        <i className="fas fa-times" onClick={e => this.handleClose(e)}></i>
                     </div>
                 </div>
-                <div className={styles.options}>
-                    <i className="fas fa-info-circle" onClick={e => this.handleDelete(e)}></i>
-                    <i className="fas fa-times" onClick={e => this.handleClose(e)}></i>
-                </div>
+                {this.state.infoOpen && <MessageInfo user={this.props.receiver} deleteChat={this.handleDelete} />}
             </div>
         );
     }
---
+++ src/components/MessageInfo/MessageInfo.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { CHAT_DELETED } from "../../message-utils/events";
+import ChatService from "../../services/chat-service";
+import CommUnityContext from "../../contexts/context";
+import styles from "./MessageInfo.module.css";
+
+class MessageInfo extends Component {
+    static contextType = CommUnityContext;
+
+    handleGoToPost(e, post) {
+        e.preventDefault();
+        this.props.history.push(`/post/${post.id}`)
+    }
+
+    formatSentence(post, user) {
+        if (post.user_id === this.context.user.id) {
+            if (post.post_type === "offer") {
+                return `${user.first_name} offered to help you`
+            } else {
+                return `You accepted help from ${user.first_name}`
+            }
+        } else if (post.user_id === user.id) {
+            if (post.post_type === "offer") {
+                return `${user.first_name} offered to help you`
+            } else {
+                return `You offered to help ${user.first_name}`
+            }
+        }
+    }
+
+    render() {
+        const { post } = this.context.activeChat;
+        const { user } = this.props;
+        return (
+            <div className={styles.container}>
+                <p>{this.formatSentence(post, user)}</p>
+                <div className={styles.buttons}>
+                    <button type="button" onClick={e => this.handleGoToPost(e, post)}>Go to post</button>
+                    <button type="button" onClick={e => this.props.deleteChat(e)}>Delete chat</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withRouter(MessageInfo)
