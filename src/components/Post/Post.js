73b86ab45f4ededf689e5f09b0ef704d1c069717
zoeferencova <zoeferencova@gmail.com>
2020-08-06 16:29:26 -0400

    fixed form width issues for mobile

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -15,21 +15,24 @@ class Post extends Component {
                 <div className={styles.details}>
                     <div>
                         <div className={styles.postHeader}>
-                            <ProfilePicture first_name={this.props.first_name} user_name={this.context.user.first_name} />
-                            <h4>{this.props.first_name} {this.props.post_type === "offer" ? "offered to help" : "requested help"}</h4>
+                            <div className={styles.nameSection}>
+                                <ProfilePicture first_name={this.props.first_name} user_name={this.context.user.first_name} />
+                                <h4>{this.props.first_name} {this.props.post_type === "offer" ? "offered to help" : "requested help"}</h4>
+                            </div>
+                            <div>
+                                <span>{this.props.post_type === "offer" ? <i className={`fas fa-heart ${styles.heart}`}></i> : <i className={`fas fa-hand-paper ${styles.hand}`}></i>}</span>
+                                <span className={styles.distance}>{UserDataService.metersToMiles(this.props.distance_from_user) + " mi"}</span>
+                            </div>
                         </div>
                         <ul className={styles.helpItems}>
                             {this.props.categories.map(task => <Task key={task} task={task} />)}
                         </ul>
-                        <p>{this.props.description}</p>
+                        <p className={styles.description}>{this.props.description}</p>
                     </div>
-                    <div className={styles.right}>
-                        <div>
-                            <span>{this.props.post_type === "offer" ? <i className={`fas fa-heart ${styles.heart}`}></i> : <i className={`fas fa-hand-paper ${styles.hand}`}></i>}</span>
-                            <span className={styles.distance}>{UserDataService.metersToMiles(this.props.distance_from_user) + " mi"}</span>
-                        </div>
+                    {/* <div className={styles.right}>
+                        
                         {this.props.post_type === "request" && <span className={styles[this.props.urgency]}><i className="fas fa-circle"></i> {this.props.urgency} urgency</span>}
-                    </div>
+                    </div> */}
                     
                 </div>
                 <ButtonLight type="button" onClick={() => this.props.history.push(`/post/${this.props.id}`)} className={styles.buttonLink}>{this.props.post_type === "offer" ? "Accept offer" : "Offer to help"}</ButtonLight>
