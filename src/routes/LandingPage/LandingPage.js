ec97ff89686b8353a0e498d3efb027c8ca85f836
zoeferencova <zoeferencova@gmail.com>
2020-09-16 15:14:58 -0400

    added final content and basic html structure to landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -8,24 +8,53 @@ class LandingPage extends Component {
         return (   
             <main>
                 <header className={styles.header}>
-                    <h1>Coming together in a time of need</h1>
-                    <p>Right now, more than ever, we need to unite our communities and help those in need. CommUnity is a free platform that enables the sharing of services and resources to ensure no one is left stranded during the Coronavirus (COVID-19) outbreak.</p>
-                    <button type="button" onClick={() => this.props.history.push("/register")}>Start Now</button>
+                    <div className={styles.left}>
+                        <h1 className={styles.heading}>Coming together in a time of need</h1>
+                        <p className={styles.subheading}>Right now, more than ever, we need to unite our communities and help those in need. CommUnity is a free platform that enables the sharing of services and resources to ensure no one is left stranded during the Coronavirus (COVID-19) outbreak.</p>
+                        <button type="button" onClick={() => this.props.history.push("/register")}>Start now</button>
+                        <button type="button" onClick={() => this.props.history.push("/demo")}>See a demo</button>
+                    </div>
+                    <div className={styles.right}>
+                        {/* image here */}
+                    </div>
                 </header>
                 <section className={styles.section}>
-                    <h2>How does it work?</h2>
-                    <p>[<em>placeholder for screenshot of main feed page</em>]</p>
-                    <p>CommUnity lets users post offers or requests for help and see other active postings in their neighborhood.</p>
+                    <div className={styles.left}>
+                        <h2 className={styles.heading}>How does it work?</h2>
+                        <p className={styles.subheading}>CommUnity lets users post offers or requests for help and see other active postings in their neighborhood. Just set your location and neighborhood radius and create a post. Other members of your community can then offer to help or accept help which will initiate a private chat where you can securely work out the details.</p>
+                    </div>
+                    <div className={styles.right}>
+                        {/* mockup here */}
+                    </div>
+                </section>
+                <section className={`${styles.section} ${styles.icons}`}>
+                    <h2>What our helpers can do for you</h2>
+                    <div className={styles.iconList}>
+                        <div className={styles.iconSection}>
+                            {/* icon here */}
+                            <h3 className={styles.iconHeading}>Picking up supplies</h3>
+                            <p className={styles.iconSubheading}>Running low on groceries, medicine, or household supplies? Our helpers can grab anything you need and drop them off at your doorstep.</p>
+                        </div>
+                        <div className={styles.iconSection}>
+                            {/* icon here */}
+                            <h3 className={styles.iconHeading}>Dog walking</h3>
+                            <p className={styles.iconSubheading}>Our helpers can take your furry friend for a safe stroll if you don’t feel safe being out and about.</p>
+                        </div>
+                        <div className={styles.iconSection}>
+                            {/* icon here */}
+                            <h3 className={styles.iconHeading}>Friendly chat</h3>
+                            <p className={styles.iconSubheading}>We know that quarantining can be an isolating and frightening time. Our helpers can offer a friendly chat through our secure instant messaging system.</p>
+                        </div>
+                    </div>
                 </section>
                 <section className={styles.section}>
-                    <h2>What can our helpers help with?</h2>
-                    <p>[<em>placeholder for graphic related to various services offered</em>]</p>
-                    <p>Our helpers can assist with picking up supplies, running errands, or even just a friendly phone call or chat.</p>
-                </section>
-                <section className={styles.section}>
-                    <h2>Safety and security are important to us</h2>
-                    <p>[<em>placeholder for screenshot of chat/map</em>]</p>
-                    <p>Your exact address will never be shared with other users, just your first name and neighborhood. Once matched with a helper or person requesting help, you will enter a private chat with the user where you can securely discuss details of the request.</p>
+                    <div className={styles.left}>
+                        {/* mockup here */}
+                    </div>
+                    <div className={styles.right}>
+                    <h2 className={styles.heading}>Your safety and security is important to us</h2>
+                        <p className={styles.subheading}>We will never share any of your personal information. The other users in your neighborhood will only see your first name and location radius (not your exact location) so the amount of information that you share in the chat is 100% your call.</p>
+                    </div>
                 </section>
                 <footer className={styles.footer}>Footer</footer>
             </main>
