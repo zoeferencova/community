a5f4584f8a8a5b9726481456f28e3dc512c5140c
zoeferencova <zoeferencova@gmail.com>
2020-09-18 16:06:02 -0400

    fixed deactivation success page styling

--- src/routes/DeactivationSuccessPage/DeactivationSuccessPage.js
+++ src/routes/DeactivationSuccessPage/DeactivationSuccessPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
+import { ButtonDark } from "../../components/Utils/Utils";
+
+import styles from "./DeactivationSuccessPage.module.css";
+
 class DeactivationSuccessPage extends Component {
     render() {
         return ( 
-            <div>
+            <div className={styles.main}>
                 <h3>Your account has been successfully deactivated</h3>
-                <button onClick={() => this.props.history.push("/")}>Go to Home Page</button>
+                <ButtonDark onClick={() => this.props.history.push("/")}>Go to Home Page</ButtonDark>
             </div>  
         )
     }
