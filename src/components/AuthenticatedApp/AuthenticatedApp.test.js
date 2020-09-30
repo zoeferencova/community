6022d872d4228316ca5617dfff97ab049dd7e71b
zoeferencova <zoeferencova@gmail.com>
2020-09-30 19:26:27 -0400

    finished AuthenticatedApp conditional rendering tests

--- src/components/AuthenticatedApp/AuthenticatedApp.test.js
+++ src/components/AuthenticatedApp/AuthenticatedApp.test.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import AuthenticatedApp from "./AuthenticatedApp";
+import { BrowserRouter } from "react-router-dom";
 
 
 describe("AuthenticatedApp", () => {
-  it("renders AuthenticatedApp component", () => {
-    const wrapper = shallow(<AuthenticatedApp />);
+    let wrapper;
 
-    expect(wrapper).toMatchSnapshot();
-  });
+    beforeEach(() => {
+        wrapper = shallow(<AuthenticatedApp  />);
+    });
+
+    it("renders AuthenticatedApp component", () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("only renders the Nav component when the logged in user has a first_name value", () => {
+        wrapper = mount(
+            <BrowserRouter>
+                <AuthenticatedApp />
+            </BrowserRouter>
+        );
+
+        wrapper.find("AuthenticatedApp").setState({ user: { first_name: "Zoe" } });
+        expect(wrapper.find("Nav").length).toBe(1);
+
+        wrapper.find("AuthenticatedApp").setState({ user: {} });
+        expect(wrapper.find("Nav").length).toBe(0);
+    });
 });
 
 
