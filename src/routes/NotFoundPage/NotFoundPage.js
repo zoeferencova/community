5cfde527469d366603f2689632cd015b87cb3405
zoeferencova <zoeferencova@gmail.com>
2023-04-07 17:47:49 +0900

    deleted unused images

--- src/routes/NotFoundPage/NotFoundPage.js
+++ src/routes/NotFoundPage/NotFoundPage.js
@@ -14,7 +14,6 @@ const NotFoundPage = ({ isLoggedIn }) => {
     return (
         <>
             <div className={(window.location.pathname === "/location" && isLoggedIn) ? styles.hide : styles.notFound}>
-                <img src={require("../../images/not-found.png")} alt="Not found error" className={styles.image}></img>
                 <h1 className={styles.title}>Sorry, that page couldn't be found</h1>
                 <ButtonLight onClick={redirect}>Take me home</ButtonLight>
             </div>
