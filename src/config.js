13686231eb47ba5051ffb0db575d842f56274462
zoeferencova <zoeferencova@gmail.com>
2023-04-20 06:24:21 -0400

    updated db urls

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    // API_ENDPOINT: process.env.REACT_APP_BASE_URL,
-    // SOCKET_URL: process.env.REACT_APP_SOCKET_URL,
-    API_ENDPOINT: "http://localhost:8000/api",
-    SOCKET_URL: "http://localhost:8000",
+    API_ENDPOINT: process.env.REACT_APP_BASE_URL,
+    SOCKET_URL: process.env.REACT_APP_SOCKET_URL,
+    // API_ENDPOINT: "http://localhost:8000/api",
+    // SOCKET_URL: "http://localhost:8000",
     TOKEN_KEY: process.env.TOKEN_KEY,
     GMAPS_API_KEY: process.env.REACT_APP_GMAP_API_KEY,
     GMAPS_LIBRARIES: ["places"]
